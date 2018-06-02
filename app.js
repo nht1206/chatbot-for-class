550af381493894fea57a32b808b01e3327e4a625
nht1206 <kenhnhttv@gmail.com>
2018-06-02 17:05:22 +0700

    make it better

---
+++ app.js
@@ -0,0 +1,43 @@
+//app.js
+const express = require('express')
+const translate = require('google-translate-api')
+const cookie = require('cookie-parser')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+let port = process.env.PORT || 3000
+
+
+app.use(morgan('dev'))
+app.use(cookie())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+app.get('/api/translate/', (req, res) => {
+    let text = req.param('text')
+    let to = req.param('to')
+    translate(text, { to : to}).then((data) => {
+        if (data.from.text.value) {
+            var result = {
+                "messages": [
+                  {"text": data.text},
+                  {"text": "Sai lỗi chính tả: " + data.from.text.value}
+                ]
+            }
+        } else {
+            var result = {
+                "messages": [
+                    {"text": data.text}
+                ]
+            }
+        }
+        res.send(result)
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port: ', port)
+})
