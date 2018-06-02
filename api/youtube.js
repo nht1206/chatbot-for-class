12dc75621fbc2d1fb1d962794a8c0ccb0d68d07c
nht1206 <kenhnhttv@gmail.com>
2018-06-02 22:40:57 +0700

    make it better

--- api/youtube.js
+++ api/youtube.js
@@ -2,23 +2,36 @@ const fs = require('fs')
 const ytdl = require('ytdl-core')
 module.exports = (app) => {
     app.get('/api/youtube/', (req, res) => {
-        let url = req.param('url')
+        /*let url = req.param('url')
         let isValiUrl = dytdl.validateURL(url)
         if (isValiUrl) {
             ytdl(url, { filter: (format) => format.container === 'mp4' })
                 .pipe(fs.createWriteStream('public/video.mp4'))
             var result = {
                 "messages": [
-                    {
-                        "attachment": {
-                            "type": "video",
-                            "payload": {
-                                "url": "https://rockets.chatfuel.com/assets/video.mp4"
-                            }
-                        }
+                  {
+                    "attachment": {
+                      "type": "video",
+                      "payload": {
+                        "url": "https://bot-tpm6.herokuapp.com/videos/video.mp4"
+                      }
                     }
+                  }
                 ]
-            }
+              }
+            res.send(result)*/
+            var result = {
+                "messages": [
+                  {
+                    "attachment": {
+                      "type": "video",
+                      "payload": {
+                        "url": "https://bot-tpm6.herokuapp.com/videos/video.mp4"
+                      }
+                    }
+                  }
+                ]
+              }
             res.send(result)
         })
 }
