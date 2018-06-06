bcf2ae9f07e8d8ad7300d9792261b9eaabf653cd
kenhnhttv@gmail.com <kenhnhttv@gmail.com>
2018-06-07 02:26:16 +0700

    make it better

--- public/test.js
+++
@@ -1,7 +0,0 @@
-const ffmpeg = require('fluent-ffmpeg')
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
-ffmpeg.setFfmpegPath(ffmpegPath)
-ffmpeg({ source: 's/video.mp4' })
-.withAudioCodec('libmp3lame')
-.toFormat('mp3')
-.save('audio.mp3')
