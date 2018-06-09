5bf9db0ec9004650eebe2cd2957220720c043d42
kenhnhttv@gmail.com <kenhnhttv@gmail.com>
2018-06-10 02:12:45 +0700

    make it better

--- api/translate.js
+++ api/translate.js
@@ -1,4 +1,5 @@
 const translate = require('google-translate-api')
+const request = require('request')
 module.exports = (app) => {
     app.get('/api/translate/', (req, res) => {
         let text = req.param('text')
@@ -40,6 +41,18 @@ module.exports = (app) => {
             res.send(result)
         }).catch((err) => {
             console.log(err)
+        })
+    })
+    app.get('/test', (req, res) => {
+        //request.post('https://api.chatfuel.com/bots/5b116878e4b0be54cc171eea/users/100007104076512/send?chatfuel_token=75NBThzxDIscaXBwrbCVOkdFSFlC1Ud7Chtrh26uGFd3xZQLRzj3OD7BfCXg0dJ0&chatfuel_message_tag=SHIPPING_UPDATE&chatfuel_block_id=5b1c0b33e4b0206f53bd379b')
+        //.form({
+        //    test: 'haha'
+        //})
+        request.post({url:'https://api.chatfuel.com/bots/5b116878e4b0be54cc171eea/users/100007104076512/send?chatfuel_token=75NBThzxDIscaXBwrbCVOkdFSFlC1Ud7Chtrh26uGFd3xZQLRzj3OD7BfCXg0dJ0&chatfuel_message_tag=SHIPPING_UPDATE&chatfuel_block_id=5b1c0b33e4b0206f53bd379b', form: {
+            test: 'haha'
+            }
+        }, function(err,httpResponse,body){ 
+            res.send(body)
         })
     })
 }
