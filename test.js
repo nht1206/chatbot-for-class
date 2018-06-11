6528e242af9cffd85c925d6c17e5986ad713afae
kenhnhttv@gmail.com <kenhnhttv@gmail.com>
2018-06-11 20:56:56 +0700

    update chat with strangers

--- test.js
+++
@@ -1,9 +0,0 @@
-const Users = require('./models/users')
-let newUser = new Users({
-    userId: '11',
-    strangersID: '',
-    status: false,
-    rela: false,
-    gender: 'male'
-})
-newUser.save()
