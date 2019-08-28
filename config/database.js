07384270245f192d788359b4740b14fbd5eab1a3
nht1206 <35588527+nht1206@users.noreply.github.com>
2019-08-28 22:22:47 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //config/database.js
-let userDB = 'chat_bot'
-let passDB = 'nht1206'
+let userDB = ''
+let passDB = ''
 const urlConnect = `mongodb://${userDB}:${passDB}@ds153890.mlab.com:53890/chat_bot`
 let getUrl = () => {
     return urlConnect
